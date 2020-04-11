9bb70a7c8f845305fcfb8e4e292df6e6fc178fb4
seema1611 <seemarajpure16@gmail.com>
2020-04-11 14:01:57 +0530

    ADDED: Function to check email valid or NOT

--- emailValidation.sh
+++ emailValidation.sh
@@ -4,8 +4,11 @@ shopt -s extglob
 #Patterns for check valid email address
 first_Email_Pattern="^[a-z0-9]{1,}$"
 second_Email_Pattern="^[@]{1}[a-z0-9]{1,}$"
-third_Email_Pattern="^[.]{1}[a-z]{2,4}$"
+third_Email_Pattern="^[.]{1}[a-z]{2,4}([.]{1}[a-z]{2,4}+)?$"
 four_Email_Pattern="^([.|_|+|-]?[a-z0-9]+)?$"
+
+#Pattern of check hole email address
+email_Pattern="^[a-z0-9]{1,}([.|_|+|-]?[a-z0-9]+)?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]{1}[a-z]{2,4}+)?$"
 
 #Function to check valid email address
 function emailValidation() {
@@ -18,14 +21,6 @@ function emailValidation() {
 		echo "Invalid, Email Address"
 	fi
 }
-read -p "Enter the first part of email address: " email_Address
-emailValidation $email_Address $first_Email_Pattern
+read -p "Enter the email address: " email_Address
+emailValidation $email_Address $email_Pattern
 
-read -p "Enter the email address after [@] symbol: " email_Address
-emailValidation $email_Address $second_Email_Pattern
-
-read -p "Enter the email address after [.] symbol: " email_Address
-emailValidation $email_Address $third_Email_Pattern
-
-read -p "Enter the email address for special symbol like [_+-]: " email_Address
-emailValidation $email_Address $four_Email_Pattern
